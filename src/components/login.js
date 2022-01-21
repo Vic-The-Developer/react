e5d1b4c24094f8eb2c48f11eed39b3c94bdbe87b
Victor Mwendwa <83487428+Vic-The-Developer@users.noreply.github.com>
2022-01-21 18:08:29 +0300

    Update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -5,7 +5,7 @@ import {useGoogleLogin} from 'react-use-googlelogin';
 import {useGoogleAuth} from '../context'
 require('cors');
 
-const clientId = '250874539293-jim8v3d7039nocm1lsli7a3udikua8gn.apps.googleusercontent.com';
+const clientId = '';
 function Login(props){
     const {signIn} = useGoogleAuth();
     return(
